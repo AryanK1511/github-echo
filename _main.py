8b41f4b796304ddc1d76337721a5818f144acc97
AryanK1511 <aryankhurana1511@gmail.com>
2024-09-06 19:43:27 -0400

    Entrypoint works with all features

--- _main.py
+++ _main.py
@@ -9,7 +9,11 @@ from typing_extensions import Annotated
 
 from application.utils.callbacks import process_tasks, version_callback
 
-console = Console()
+# Console instances for standard and error output
+console = Console(soft_wrap=True)
+err_console = Console(stderr=True, soft_wrap=True)
+
+# Initialize the Typer app
 app = typer.Typer(
     no_args_is_help=True,
     help="CLI tool built to obtain in-depth, actionable information about GitHub repositories.",
@@ -44,7 +48,15 @@ def github_repo_insights(
         version (Optional[bool]): Optional. If provided, prints the version number and exits the application.
         output_file (Optional[Path]): Optional. If provided, the path to a file where the Markdown summary will be saved.
     """
-    process_tasks(github_repository_url, output_file)
+    try:
+        process_tasks(github_repository_url, output_file)
+    except Exception as e:
+        err_console.print("\n[bold red]🚨 Something went wrong![/bold red]\n")
+        err_console.print(f"[red]💡 [bold]Error:[/bold] {e}\n", highlight=True)
+        err_console.print(
+            "[bold yellow]Tip:[/bold yellow] Ensure the GitHub URL is valid and check your internet connection.\n"
+        )
+        raise typer.Exit(code=1)
 
 
 if __name__ == "__main__":
