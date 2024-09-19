796ffa1c039b435b9d9b41fc04ce6856b5fd1dc6
Aryan Khurana <aryankhurana1511@gmail.com>
2024-09-19 14:53:01 -0400

    Fix typo in test

--- tests/unit/test_cli_features.py
+++ tests/unit/test_cli_features.py
@@ -14,4 +14,4 @@ def test_version(run_command: callable) -> None:
     result: subprocess.CompletedProcess = run_command("-v")
 
     assert result.returncode == 0
-    assert "github-echo version 0.1.1" in result.stdout
+    assert "github-echo version 0.2.0" in result.stdout
