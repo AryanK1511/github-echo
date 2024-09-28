7f7889664b8537ad2f5dfbde615eeb08adc1e201
Aryan Khurana <aryankhurana1511@gmail.com>
2024-09-28 16:34:41 -0400

    Add error handling

--- application/core/github_api.py
+++ application/core/github_api.py
@@ -1,6 +1,9 @@
 import asyncio
 from typing import Any, Dict
 
+import httpx
+import typer
+
 from application.utils.api import query_github
 
 
@@ -9,24 +12,15 @@ async def fetch_github_data(owner: str, repo: str) -> Dict[str, Any]:
     """
     Fetches and combines various data points about a GitHub repository using the GitHub API.
 
-    This asynchronous function gathers information about a specified GitHub repository by
-    making concurrent requests to various GitHub API endpoints, including metadata, commit history,
-    contributors, issues, pull requests, releases, languages, and community profile.
-
     Args:
         owner (str): The GitHub username or organization name that owns the repository.
         repo (str): The name of the repository to fetch data for.
 
     Returns:
-        Dict[str, Any]: A dictionary containing combined data from multiple GitHub API calls, organized
-                        into sections such as repository metadata, commit history, contributors, issues,
-                        pull requests, releases, languages, and community profile.
-
-    Example:
+        Dict[str, Any]: A dictionary containing combined data from multiple GitHub API calls.
     """
 
     # Run all the fetch functions concurrently
-
     results = await asyncio.gather(
         fetch_repo_metadata(owner, repo),
         fetch_commits_history(owner, repo),
@@ -37,10 +31,6 @@ async def fetch_github_data(owner: str, repo: str) -> Dict[str, Any]:
         fetch_languages(owner, repo),
         fetch_community_profile(owner, repo),
     )
-
-    # Raise an error if any of the fetch functions failed or no data was returned
-    if any(result is None for result in results):
-        raise ValueError("Failed to fetch data for the repository")
 
     # Combine the results into a single JSON object and return it
     combined_data = {
@@ -60,96 +50,220 @@ async def fetch_github_data(owner: str, repo: str) -> Dict[str, Any]:
 # Function to fetch repository metadata
 async def fetch_repo_metadata(owner: str, repo: str) -> Dict[str, Any]:
     url = f"https://api.github.com/repos/{owner}/{repo}"
-    metadata = await query_github(url)
+    try:
+        metadata = await query_github(url)
+        relevant_fields = [
+            "name",
+            "full_name",
+            "description",
+            "html_url",
+            "homepage",
+            "license",
+            "stargazers_count",
+            "watchers_count",
+            "forks_count",
+            "open_issues_count",
+            "subscribers_count",
+            "created_at",
+            "updated_at",
+            "pushed_at",
+            "size",
+            "language",
+            "topics",
+        ]
+        return {field: metadata.get(field, None) for field in relevant_fields}
 
-    relevant_fields = [
-        "name",
-        "full_name",
-        "description",
-        "html_url",
-        "homepage",
-        "license",
-        "stargazers_count",
-        "watchers_count",
-        "forks_count",
-        "open_issues_count",
-        "subscribers_count",
-        "created_at",
-        "updated_at",
-        "pushed_at",
-        "size",
-        "language",
-        "topics",
-    ]
+    except httpx.HTTPStatusError as e:
+        if e.response.status_code == 401:
+            raise typer.Exit("Unauthorized: Check your GitHub API Key.")
+        elif e.response.status_code == 404:
+            raise typer.Exit(f"Repository '{owner}/{repo}' not found.")
+        else:
+            raise typer.Exit(
+                f"Error fetching metadata: {e.response.status_code} - {e.response.text}"
+            )
 
-    return {field: metadata.get(field, None) for field in relevant_fields}
+    except Exception as e:
+        raise typer.Exit(
+            f"Failed to fetch metadata for repository '{owner}/{repo}': {e}"
+        )
 
 
 # Function to fetch commits history
 async def fetch_commits_history(owner: str, repo: str) -> Dict[str, Any]:
     url = f"https://api.github.com/repos/{owner}/{repo}/commits"
-    commits = await query_github(url)
+    try:
+        commits = await query_github(url)
+        relevant_fields = ["sha", "author", "committer", "message", "url"]
 
-    relevant_fields = ["sha", "author", "committer", "message", "url"]
+        return [
+            {
+                field: (
+                    commit.get("commit", {}).get(field, None)
+                    if field in ["author", "committer"]
+                    else commit.get(field, None)
+                )
+                for field in relevant_fields
+            }
+            for commit in commits
+        ]
 
-    return [
-        {
-            field: (
-                commit.get("commit", {}).get(field, None)
-                if field in ["author", "committer"]
-                else commit.get(field, None)
+    except httpx.HTTPStatusError as e:
+        if e.response.status_code == 401:
+            raise typer.Exit("Unauthorized: Check your GitHub API Key.")
+        elif e.response.status_code == 404:
+            raise typer.Exit(f"Commits not found for repository '{owner}/{repo}'.")
+        else:
+            raise typer.Exit(
+                f"Error fetching commit history: {e.response.status_code} - {e.response.text}"
             )
-            for field in relevant_fields
-        }
-        for commit in commits
-    ]
+
+    except Exception as e:
+        raise typer.Exit(
+            f"Failed to fetch commit history for repository '{owner}/{repo}': {e}"
+        )
 
 
 # Function to fetch contributors
 async def fetch_contributors(owner: str, repo: str) -> Dict[str, Any]:
     url = f"https://api.github.com/repos/{owner}/{repo}/contributors"
-    contributors = await query_github(url)
+    try:
+        contributors = await query_github(url)
+        relevant_fields = ["login", "id", "avatar_url", "html_url", "contributions"]
 
-    relevant_fields = ["login", "id", "avatar_url", "html_url", "contributions"]
+        return [
+            {field: contributor.get(field, None) for field in relevant_fields}
+            for contributor in contributors
+        ]
 
-    return [
-        {field: contributor.get(field, None) for field in relevant_fields}
-        for contributor in contributors
-    ]
+    except httpx.HTTPStatusError as e:
+        if e.response.status_code == 401:
+            raise typer.Exit("Unauthorized: Check your GitHub API Key.")
+        elif e.response.status_code == 404:
+            raise typer.Exit(f"Contributors not found for repository '{owner}/{repo}'.")
+        else:
+            raise typer.Exit(
+                f"Error fetching contributors: {e.response.status_code} - {e.response.text}"
+            )
+
+    except Exception as e:
+        raise typer.Exit(
+            f"Failed to fetch contributors for repository '{owner}/{repo}': {e}"
+        )
 
 
 # Function to fetch issues
 async def fetch_issues(owner: str, repo: str) -> Dict[str, Any]:
     url = f"https://api.github.com/repos/{owner}/{repo}/issues"
-    return await query_github(url)
+    try:
+        issues = await query_github(url)
+        return issues
+
+    except httpx.HTTPStatusError as e:
+        if e.response.status_code == 401:
+            raise typer.Exit("Unauthorized: Check your GitHub API Key.")
+        elif e.response.status_code == 404:
+            raise typer.Exit(f"Issues not found for repository '{owner}/{repo}'.")
+        else:
+            raise typer.Exit(
+                f"Error fetching issues: {e.response.status_code} - {e.response.text}"
+            )
+
+    except Exception as e:
+        raise typer.Exit(f"Failed to fetch issues for repository '{owner}/{repo}': {e}")
 
 
 # Function to fetch pull requests
 async def fetch_pull_requests(owner: str, repo: str) -> Dict[str, Any]:
     url = f"https://api.github.com/repos/{owner}/{repo}/pulls"
+    try:
+        pulls = await query_github(url)
+        return pulls
 
-    return await query_github(url)
+    except httpx.HTTPStatusError as e:
+        if e.response.status_code == 401:
+            raise typer.Exit("Unauthorized: Check your GitHub API Key.")
+        elif e.response.status_code == 404:
+            raise typer.Exit(
+                f"Pull requests not found for repository '{owner}/{repo}'."
+            )
+        else:
+            raise typer.Exit(
+                f"Error fetching pull requests: {e.response.status_code} - {e.response.text}"
+            )
+
+    except Exception as e:
+        raise typer.Exit(
+            f"Failed to fetch pull requests for repository '{owner}/{repo}': {e}"
+        )
 
 
 # Function to fetch releases
 async def fetch_releases(owner: str, repo: str) -> Dict[str, Any]:
     url = f"https://api.github.com/repos/{owner}/{repo}/releases"
+    try:
+        releases = await query_github(url)
+        return releases
 
-    # Return data for all releases
-    return await query_github(url)
+    except httpx.HTTPStatusError as e:
+        if e.response.status_code == 401:
+            raise typer.Exit("Unauthorized: Check your GitHub API Key.")
+        elif e.response.status_code == 404:
+            raise typer.Exit(f"Releases not found for repository '{owner}/{repo}'.")
+        else:
+            raise typer.Exit(
+                f"Error fetching releases: {e.response.status_code} - {e.response.text}"
+            )
+
+    except Exception as e:
+        raise typer.Exit(
+            f"Failed to fetch releases for repository '{owner}/{repo}': {e}"
+        )
 
 
 # Function to fetch languages used in the repository
 async def fetch_languages(owner: str, repo: str) -> Dict[str, Any]:
     url = f"https://api.github.com/repos/{owner}/{repo}/languages"
+    try:
+        languages = await query_github(url)
+        return languages
 
-    # Return the complete language data
-    return await query_github(url)
+    except httpx.HTTPStatusError as e:
+        if e.response.status_code == 401:
+            raise typer.Exit("Unauthorized: Check your GitHub API Key.")
+        elif e.response.status_code == 404:
+            raise typer.Exit(f"Languages not found for repository '{owner}/{repo}'.")
+        else:
+            raise typer.Exit(
+                f"Error fetching languages: {e.response.status_code} - {e.response.text}"
+            )
+
+    except Exception as e:
+        raise typer.Exit(
+            f"Failed to fetch languages for repository '{owner}/{repo}': {e}"
+        )
 
 
 # Function to fetch community profile
 async def fetch_community_profile(owner: str, repo: str) -> Dict[str, Any]:
     url = f"https://api.github.com/repos/{owner}/{repo}/community/profile"
+    try:
+        community_profile = await query_github(url)
+        return community_profile
 
-    # Return the complete community profile
-    return await query_github(url)
+    except httpx.HTTPStatusError as e:
+        if e.response.status_code == 401:
+            raise typer.Exit("Unauthorized: Check your GitHub API Key.")
+        elif e.response.status_code == 404:
+            raise typer.Exit(
+                f"Community profile not found for repository '{owner}/{repo}'."
+            )
+        else:
+            raise typer.Exit(
+                f"Error fetching community profile: {e.response.status_code} - {e.response.text}"
+            )
+
+    except Exception as e:
+        raise typer.Exit(
+            f"Failed to fetch community profile for repository '{owner}/{repo}': {e}"
+        )
