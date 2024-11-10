98a7c74e4161e43ca80f869368cf224b5fa96848
Aryan Khurana <aryankhurana1511@gmail.com>
2024-11-10 09:54:55 -0500

    fix: CI pipeline

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,8 @@
-from fixtures import run_command  # noqa: F401
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def set_dummy_env_vars(monkeypatch):
+    monkeypatch.setenv('google_gemini_api_key', 'Sample')
+    monkeypatch.setenv('github_api_token', 'Sample')
+    monkeypatch.setenv('groq_api_key', 'Sample')
